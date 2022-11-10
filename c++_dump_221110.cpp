6200df731daa0055c85f9e912d19d216ceee4e57
teokim0728 <117898966+teokim0728@users.noreply.github.com>
2022-11-10 17:34:55 +0900

    Create c++_dump_221110.cpp

---
+++ c++_dump_221110.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[3][10];
+    int i,j;
+    for (int i=0; i<3; i++)
+        gets(a[i]);
+    for (i=0;i<3; i++){
+        for (j=0; a[i][j]!='\0'; j++)
+            printf("%2c", a[i][j]);
+        printf("\n");
+    }
+}
